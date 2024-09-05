3bc72bbd305451d38647352cd54fc16d3a9aad4e
anilkr21 <anilkumargangarapu01@gmail.com>
2024-09-05 14:19:24 +0530

    gh-pages installed again after charts addition

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 const Bar = () =>{
     return(
         <Box m="20px">
-            <Header title="BAR CHART" subtitle="Simple Bar Chart" />
+            <Header title="Bar Chart" subtitle="Simple Bar Chart" />
             <Box height="75vh" >
                 <BarChart />
             </Box>
